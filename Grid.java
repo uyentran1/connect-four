c62abe46477699bd726c59494b02ce01bc37605c
Uyen Tran <tranphuonguyen1011@gmail.com>
2024-11-09 13:36:13 +0000

    Update toString method using StringBuilder for readability

--- Grid.java
+++ Grid.java
@@ -1,23 +1,24 @@
 public class Grid {
     final static int HEIGHT = 6;
     final static int WIDTH = 7;
+    private static final char EMPTY_CELL = ' ';
     private char[][] grid;
 
     public Grid() {
-        // Initialise a new grid
+        // Initialise a new grid with empty cells
         this.grid = new char[HEIGHT][WIDTH];
         for (char[] row : grid) {
             for (int col = 0; col < row.length; col++) {
-                row[col] = ' ';
+                row[col] = EMPTY_CELL;
             }
         }
     }
 
-    // Check if grid is full
+    // Check if the grid is full
     public boolean isFull() {
         for (char[] row : grid) {
             for (char cell : row) {
-                if (cell == ' ') {
+                if (cell == EMPTY_CELL) {
                     return false;
                 }
             }
@@ -25,22 +26,16 @@ public class Grid {
         return true;
     }
 
-    // Check if a column is full. Parameter: player's column
-    public boolean columnIsFull(int playersColumn) {
-        // Grid column starts from 0, Player's column starts from 1
-        int gridColumn = playersColumn - 1;
-        for (char[] row : grid) {
-            if (row[gridColumn] == ' ') {
-                return false;
-            }
-        }
-        return true;
+    // Check if a column is full. Parameter: player input column (starts from 1)
+    public boolean columnIsFull(int playerInputColumn) {
+        int gridColumn = playerInputColumn - 1;
+        return grid[0][gridColumn] != EMPTY_CELL;
     }
 
-    // Update a new disc into the grid. The disc goes to the first empty row
-    public void getDisc(Disc disc) {
+    // Place a new disc in the first empty row of the specified column
+    public void placeDisc(Disc disc) {
         for (int i = grid.length - 1; i >= 0; i--) {
-            if (grid[i][disc.getGridColumn()] == ' ') {
+            if (grid[i][disc.getGridColumn()] == EMPTY_CELL) {
                 grid[i][disc.getGridColumn()] = disc.getSymbol();
                 disc.setRow(i);
                 break;
@@ -48,78 +43,65 @@ public class Grid {
         }
     }
 
-    // Check if game has a winner after the latest disc
-    // Return true either when there is a winning situation or grid is full (a tie)
-    // Return false if there is no winning situation or grid
+    // Check for a winning condition after placing the latest disc
     public boolean haveWinner(Disc latestDisc) {
-        // Winning situations comprise horizontal, vertial and diagonal checks
-        // Horizontal check: check on the row of lastest disc, if 4 disc in a row have the same symbol
-        // 4 possibilities of winning discs' columns: 0-1-2-3, 1-2-3-4, 2-3-4-5, 3-4-5-6
-        for (int col = 0; col <= 3; col++) {
-            if (grid[latestDisc.getRow()][col] == latestDisc.getSymbol() && 
-            grid[latestDisc.getRow()][col + 1] == latestDisc.getSymbol() &&
-            grid[latestDisc.getRow()][col + 2] == latestDisc.getSymbol() && 
-            grid[latestDisc.getRow()][col + 3] == latestDisc.getSymbol()) {
+        int row = latestDisc.getRow();
+        int col = latestDisc.getGridColumn();
+        char symbol = latestDisc.getSymbol();
+
+        // Define directions as row and column increments: {rowIncrement, colIncrement}
+        int[][] directions = {
+            {0, 1}, // Horizontal
+            {1, 0}, // Vertical
+            {-1, 1}, // Diagonal (bottom-left to top-right)
+            {1, 1} // Diagonal (top-left to bottom-right)
+        };
+
+        // Check each direction for a winning line
+        for (int[] direction : directions) {
+            if (checkLine(row, col, direction[0], direction[1], symbol)) {
                 return true;
             }
         }
-    
-        // Vertical check: latest disc is is on row 0-2 and 3 discs below latest disc have the same symbol
-        if (latestDisc.getRow() <= 2 && 
-        grid[latestDisc.getRow() + 1][latestDisc.getGridColumn()] == latestDisc.getSymbol() && 
-        grid[latestDisc.getRow() + 2][latestDisc.getGridColumn()] == latestDisc.getSymbol() && 
-        grid[latestDisc.getRow() + 3][latestDisc.getGridColumn()] == latestDisc.getSymbol()) {
-            return true;
-        }
         
-        // Diagonal check (bottom-left to top-right)
-        // if latestDisc in col 0 and min row = 3, check upper
-        if (latestDisc.getGridColumn() == 0 && latestDisc.getRow() >= 3) {
-            if (grid[latestDisc.getRow() - 1][latestDisc.getGridColumn() + 1] == latestDisc.getSymbol() &&
-            grid[latestDisc.getRow() - 2][latestDisc.getGridColumn() + 2] == latestDisc.getSymbol() &&
-            grid[latestDisc.getRow() - 3][latestDisc.getGridColumn() + 3] == latestDisc.getSymbol()) {
-                return true;
+        return false;
+    }
+
+    // Helper method to check for four consecutive symbols in a given direction
+    private boolean checkLine(int row, int col, int rowIncrement, int colIncrement, char symbol) {
+        int count = 0;
+
+        // Check both directions along the line
+        for (int i = -3; i <= 3; i++) {
+            int r = row + i * rowIncrement;
+            int c = col + i * colIncrement;
+            if (r >= 0 && r < HEIGHT && c >= 0 && c < WIDTH && grid[r][c] == symbol) {
+                count++;
+                if (count == 4) return true;
+            } else {
+                count = 0;
             }
-        }
-        // if latestDisc in col 6 and max row = 2, check lower
-        if (latestDisc.getGridColumn() == 6 && latestDisc.getRow() <= 2) {
-            if (grid[latestDisc.getRow() + 1][latestDisc.getGridColumn() - 1] == latestDisc.getSymbol() &&
-            grid[latestDisc.getRow() + 2][latestDisc.getGridColumn() - 2] == latestDisc.getSymbol() &&
-            grid[latestDisc.getRow() + 3][latestDisc.getGridColumn() - 3] == latestDisc.getSymbol()) {
-                return true;
-            }
-        }
-        // if latestDisc in col 1-5, check each case diagonal if 4 consecutive discs (not out of range) same symbol
-        
-        
-        // Diagonal check (top-left to bottom-right)
-        // Check if grid is full after checking winning situation
-        if (this.isFull()) {
-            return true;
         }
 
         return false;
     }
 
     // Visualise the current state of the grid
-    // The grid visual consists of 7 rows, in which the first row indicates
-    // the column numbers, and the following rows hold players' discs
     @Override
     public String toString() {
-        String gridVisual = "";
+        StringBuilder gridVisual = new StringBuilder();
         String firstRowVisual = String.format("\n| 1 | 2 | 3 | 4 | 5 | 6 | 7 |\n");
-
-        gridVisual += firstRowVisual;
+        gridVisual.append(firstRowVisual);
 
         for (char[] row : grid) {
-            String discRowVisual = "|";
+            StringBuilder discRowVisual = new StringBuilder("|");
             for (char cell : row) {
-                discRowVisual += String.format(" %s |", cell);
+                discRowVisual.append(String.format(" %s |", cell));
             }
-            discRowVisual += "\n";
-            gridVisual += discRowVisual;
+            discRowVisual.append("\n");
+            gridVisual.append(discRowVisual);
         }
 
-        return gridVisual;
+        return gridVisual.toString();
     }
 }
